6abc3e9f9ba395939513d53b05be9c0bb6997f87
Nicole <nicolelosana@gmail.com>
2024-03-13 00:10:06 -0300

    Se modificó origin en la configuración de cors

--- config/cors.js
+++ config/cors.js
@@ -1,12 +1,11 @@
 const corsOptions = {
-    // origin: function (origin, callback) {
-    //     if (origin.startsWith('http://localhost:3001')) {
-    //         callback(null, true)
-    //     } else {
-    //         callback(new Error('Not allowed by CORS'))
-    //     }
-    // },
-    origin:"*",
+    origin: function (origin, callback) {
+        if (origin.startsWith('https://storydots-challenge-client-git-main-nicoledl.vercel.app')) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'token']
