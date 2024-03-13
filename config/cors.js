f573e96f81bfe6f806b3ba2cf7305a55b85fe141
Nicole <nicolelosana@gmail.com>
2024-03-12 23:45:00 -0300

    Se configuró el package.jsoconfig cors

--- config/cors.js
+++ config/cors.js
@@ -1,11 +1,12 @@
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (origin.startsWith('http://localhost:3001')) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
+    // origin: function (origin, callback) {
+    //     if (origin.startsWith('http://localhost:3001')) {
+    //         callback(null, true)
+    //     } else {
+    //         callback(new Error('Not allowed by CORS'))
+    //     }
+    // },
+    origin:"*",
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'token']
