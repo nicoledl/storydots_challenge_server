4a8c68c8fc72a5895b407c36c0c24304053725df
Nicole <nicolelosana@gmail.com>
2024-03-12 21:31:15 -0300

    Se modifico la conexión a sequelize

--- db/connection.js
+++ db/connection.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(database, username, password, {
 });
 
 const connectToDatabase = async () => {
+  console.log(database, username, password, host, dialect, port);
   try {
     await sequelize.authenticate();
     await sequelize.sync({ force: false });
